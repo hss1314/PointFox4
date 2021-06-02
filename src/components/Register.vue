265bf4ba46eedd70df284f4c76e726f85f723819
unknown <2652189007@qq.com>
2021-06-02 19:00:44 +0800

    前端项目06

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -6,16 +6,16 @@
           <span>用户注册</span>
         </div>
         <div id="registerContent">
-          <el-input id="username" v-model="newUser.username" clearable></el-input>
+          <el-input id="username" v-model="newUser.newusername" clearable></el-input>
           <el-input
             id="password"
-            v-model="newUser.password"
+            v-model="newUser.newpassword"
             clearable
             show-password
           ></el-input>
         </div>
         <div id="submitContent">
-          <el-button id="systemSubmit">注册</el-button>
+          <el-button id="systemSubmit" @click="registerSystem">注册</el-button>
         </div>
       </el-card>
     </div>
@@ -23,7 +23,31 @@
 </template>
 
 <script>
+import Axios from 'axios'
+const url = 'http://localhost:3000'
 export default {
+  name:"register",
+  data(){
+    return {
+            newUser:{
+                newusername:"",
+                newpassword:""
+            },
+            fullscreenLoading: false
+        }
+
+  },
+  methods:{
+    registerSystem(){
+
+      Axios.get(url+'/register',{
+        params:this.newUser
+      }).then(function
+      (resx){
+
+      })
+    }
+  },
   data() {
     return {
       newUser: {
